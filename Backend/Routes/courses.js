03676578e4a996e79f12ae8bfbe95c58e9222456
DARSHITSHAH-2906 <darshitshah501867@gmail.com>
2025-01-30 19:09:54 +0530

    Updated Course Route

--- Backend/Routes/courses.js
+++ Backend/Routes/courses.js
@@ -4,7 +4,7 @@ const { allow_login_user } = require("../Middlewares/login_user");
 
 const course_router = express.Router();
 
-course_router.get("/" ,allow_login_user, DisplayCourses);
+course_router.get("/", DisplayCourses);
 // course_router.post("/" ,allow_login_user, UploadCourse);
 course_router.post("/" ,allow_login_user, UploadCourse);
 
