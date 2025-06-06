06290b57a45c6d371b3cf39e6297b50ac2962ca0
DARSHIT SHAH <166908115+DARSHITSHAH-2906@users.noreply.github.com>
2025-06-06 13:54:12 +0530

    modified vite.config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
   server: {
     fs: {
-      strict: true, // Prevent accessing .jsx files from URL
+      strict: true,
     },
-  },
-  esbuild: {
-    loader: 'jsx',
-    include : /\.js$/,
   },
 })
